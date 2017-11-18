d3ed384efca07f4fd2b28869d21a26c8011f2855
Tanel Hiob <tanelhiob@outlook.com>
2017-11-18 22:34:14 +0200

    Improve best solution

--- TravelingSalesman.FSharp/Cities.fs
+++ TravelingSalesman.FSharp/Cities.fs
@@ -1,7 +1,8 @@
 ﻿module Cities
 
-let loadCities file =  
+let loadCities file amount =  
     System.IO.File.ReadLines file
-    |> Seq.map (fun (line:System.String) -> line.Split ([|' '|], System.StringSplitOptions.RemoveEmptyEntries) |> Array.map int)
+    |> Seq.map (fun (line:System.String) -> line.Split ([|' '|], System.StringSplitOptions.RemoveEmptyEntries) |> Array.take amount |> Array.map int)
     |> Seq.map Array.toList
+    |> Seq.take amount
     |> Seq.toList
--- TravelingSalesman.FSharp/Program.fs
+++ TravelingSalesman.FSharp/Program.fs
@@ -1,4 +1,5 @@
 ﻿open Cities
+open System.Diagnostics
 
 let rec distribute e = function
     | [] -> [[e]]
@@ -21,6 +22,8 @@ let calculateTotalDistance (cities:int list list) path =
 let findGreedyPath (cities:int list list) =
 
     let rec findGreedyPathRecursive (unvisitedCities: int list) currentCity =
+
+        printfn "greedy depth %i" (cities.Length - unvisitedCities.Length) 
 
         if unvisitedCities.Length = 1 then
             unvisitedCities
@@ -45,53 +48,74 @@ let findBestOptimalPath (cities:int list list) =
 
     let mutable bestLength = System.Int32.MaxValue
 
-    let rec findPathRec path =
+    let rec findPathRec path pathLength =
 
         if List.length path = cities.Length then
-            let possiblePath = path @ [ 0 ]
-            let distance = calculateTotalDistance cities possiblePath
+
+            let possiblePath = path @ [ List.head path ]            
+            let distance = pathLength + cities.[(List.last path)].[List.head path]
 
             if distance < bestLength then
                 bestLength <- distance
-                Some possiblePath
+                printfn "%s %i %A" (System.DateTime.Now.ToString("HH:mm:ss.ffff")) bestLength possiblePath
+                Some (possiblePath, distance)
             else
                 None
-
         else
-            let unvisitedCities = [0 .. cities.Length - 1] |> List.except path
+            let preferredUnvisitedCities =
+                [0 .. cities.Length - 1]
+                    |> List.except path
+                    |> List.map (fun city -> (city, cities.[(List.last path)].[city]))
+                    |> List.filter (fun (_, distance) -> distance < bestLength)
+                    |> List.sortBy (fun (_, distance) -> distance)
+
             let possiblePaths =
-                [ for unvisitedCity in unvisitedCities ->
-                    let possiblePath = path @ [ unvisitedCity ]
-                    let distance = calculateTotalDistance cities possiblePath
-                    if distance < bestLength then
-                        findPathRec possiblePath
-                    else
-                        None
-                ]
+                preferredUnvisitedCities
+                    |> List.map (fun (city, distance) -> findPathRec (path @ [ city ]) (distance + pathLength))
+
 
             let paths = possiblePaths |> List.filter Option.isSome |> List.map Option.get
-            if paths.IsEmpty then
+            if List.isEmpty paths then
                 None
             else
-                paths |> List.minBy (calculateTotalDistance cities) |> Some
+                paths |> List.minBy (fun (_, distance) -> distance) |> Some
+    
+    match cities.Length with
+    | len when len < 2 -> [0]
+    | _ -> fst (Option.get (findPathRec [0] 0))
 
-    Option.get (findPathRec [ 0 ])
+let takeTime operation =
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    operation
+    stopWatch.Stop()
+
+    printfn "operation took %ims" stopWatch.ElapsedMilliseconds
+
 
 [<EntryPoint>]
 let main _ = 
 
-    let cities = loadCities "Data\\eesti.in"
+    let cities = loadCities "Data\\eesti.in" 20
+    // printfn "%i" (calculateTotalDistance cities [0; 14; 2; 3; 1; 11; 13; 4; 9; 18; 17; 7; 6; 8; 19; 15; 16; 5; 10; 12; 0])
+    // 1455
+    
 
-    let greedyPath = findGreedyPath cities
-    let greedyTotalDistance = calculateTotalDistance cities greedyPath
-    printfn "%A %i" greedyPath greedyTotalDistance
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+
+    //let greedyPath = findGreedyPath cities
+    //let greedyTotalDistance = calculateTotalDistance cities greedyPath
+    //printfn "%A %i" greedyPath greedyTotalDistance
+
 
     //let bestPath = findBestPath cities
     //let bestPathTotalDistance = calculateTotalDistance cities bestPath
     //printfn "%A %i" bestPath bestPathTotalDistance
 
-    //let bestOptimalPath = findBestOptimalPath cities
-    //let bestOptimalPathTotalDistance = calculateTotalDistance cities bestPath
-    //printfn "%A %i" bestOptimalPath bestOptimalPathTotalDistance
+    let bestOptimalPath = findBestOptimalPath cities
+    let bestOptimalPathTotalDistance = calculateTotalDistance cities bestOptimalPath
+    printfn "%A %i" bestOptimalPath bestOptimalPathTotalDistance
+
+    stopWatch.Stop()
+    printfn "operation took %ims" stopWatch.ElapsedMilliseconds
 
     0 // exit gracefully
